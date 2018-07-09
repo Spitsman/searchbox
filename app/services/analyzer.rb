b40a0bed7c8d88ae3787ea9a6313ea03d01daaa9
spitsman <sanspits@yandex.ru>
2018-07-10 00:12:53 +0300

    change analyzer algorithm, add alert

--- app/services/analyzer.rb
+++ app/services/analyzer.rb
@@ -4,10 +4,25 @@ class Analyzer
       redis.rpush("analytics-#{ip}", text)
     end
 
-    def get_analytics # выбираю самый длинный запрос для каждого ip
-      all_requests = keys.map{ |key| $redis.lrange(key, 0, -1) }
-      max_lenght_requests = all_requests.map{ |ary| ary.max_by(&:length) }
-      max_lenght_requests.inject(Hash.new(0)) { |total, e| total[e] += 1; total}
+    def get_analytics
+      all_requests = keys.map{ |key| $redis.lrange(key, 0, -1) } # беру все запросы от разных ip
+
+      result = all_requests.map do |requests_for_ip| # для каждого ip удаляю запросы которые являются подстрокой других запросов
+        uniq_request_for_ip = requests_for_ip.uniq
+
+        uniq_request_for_ip.each do |el|
+          uniq_request_for_ip.each do |el2|
+            requests_for_ip.delete(el2) if el.include?(el2) && el != el2
+          end
+        end
+
+        requests_for_ip
+      end
+
+      result.flatten
+        .inject(Hash.new(0)) { |total, e| total[e] += 1; total}
+        .sort_by { |request, count| count }
+        .reverse.to_h # считаю сколько раз встретился каждый запрос
     end
 
     def destroy_all
